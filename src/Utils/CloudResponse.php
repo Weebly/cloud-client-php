ceff4d10e8196523b285a73d1de48c90bd8124e0
Raymond Chen <raymond@weebly.com>
2017-04-19 11:25:24 -0700

    silence php notices

--- src/Utils/CloudResponse.php
+++ src/Utils/CloudResponse.php
@@ -84,9 +84,9 @@ class CloudResponse
         preg_match_all("/^X-Resultset-([^:]+): ([\d]+).+$/m", $header, $p);
         $header_fields = array_combine($p[1], $p[2]);
 
-        $this->total = $header_fields['Total'] ?: -1;
-        $this->page_limit = $header_fields['Limit'] ?: -1;
-        $this->page = $header_fields['Page'] ?: -1;
+        $this->total = isset($header_fields['Total']) ? $header_fields['Total'] : -1;
+        $this->page_limit = isset($header_fields['Limit']) ? $header_fields['Limit'] : -1;
+        $this->page = isset($header_fields['Page']) ? $header_fields['Page'] : -1;
         $this->page_count = (int) ceil($this->total / $this->page_limit);
         $this->is_paginated = (($this->total) > 0);
         $this->body = $response;
